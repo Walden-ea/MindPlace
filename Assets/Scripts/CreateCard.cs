141cf8660c3980582c7037e8f044a235b75a655e
nastya nenapisannoe <nenapisannoe@yandex.ru>
2020-12-02 14:04:36 +0300

    меню, в карточках все еще где то нулльреференс вылезает

---
+++ Assets/Scripts/CreateCard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CreateCard : MonoBehaviour
+{
+   [SerializeField] GameObject cardPrefab;
+   [SerializeField] GameObject Window;
+
+   [SerializeField]InputField field;
+    void Start()
+    {
+       // Window.SetActive(false);
+    }
+
+    public void SomeMethod()
+    {
+        GameObject newCard = Instantiate(cardPrefab, new Vector3(2.0F, 0, 0), Quaternion.identity) as GameObject;
+        GameObject textSideOne = GameObject.Find("FirstSideText");
+        textSideOne.GetComponent<Text>().text = field.text;
+        //string s = field.text;
+        //textSideOne.text = "help me";
+    }
+
+    public void FillCard(GameObject card)
+    {
+        GameObject textSideOne = GameObject.Find("FirstSideText");
+        textSideOne.GetComponent<Text>().text = field.text;
+    }
+    public void Awake()
+    {
+        Window.SetActive(true);
+    }
+    public void Show(string inputString)
+    {
+
+          //inputFieldSideOne.text = inputString;
+          //Debug.Log(inputFieldSideOne.text);
+
+    }
+    void Update()
+    {
+        
+    }
+
+    void CreateNewCard()
+    {
+        
+    }
+
+
+}
