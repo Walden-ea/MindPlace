bcfe7de57828779cfb7c9c7694b3caff210d38c9
Win-Fox <70930456+Win-Fox@users.noreply.github.com>
2020-12-02 10:47:59 +0300

    hi

---
+++ Scripts/Seconds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Seconds : MonoBehaviour
+{
+  public int secondTime; // проверка отчёта секунд в Inspector
+  public float minutTime;// проверка отчёта минут в Inspector
+  private float timersecond;
+
+  public Text text;
+
+    void Update()
+    {
+      timersecond += Time.deltaTime;
+      if (timersecond >= 1) {
+          secondTime += 1;
+          timersecond = 0;
+      }
+      if (secondTime >= 60) {
+          minutTime += 1;
+          secondTime = 0;
+      }
+      if (minutTime >= 60) {
+         minutTime = 0;
+      }
+
+      text.text = $"{minutTime} : {secondTime}";
+    }
+
+}
