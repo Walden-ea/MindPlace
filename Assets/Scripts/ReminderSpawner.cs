88bfb4217d8780a4433df59c2a43cc302262ee3a
vadim <vadim-ivanov-20010510@mail.ru>
2020-12-03 20:25:58 +0300

    Added reminders

--- Assets/Scripts/ReminderSpawner.cs
+++ Assets/Scripts/ReminderSpawner.cs
@@ -2,33 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-//using System;
+using System;
 //using System.Threading;
 
 public class ReminderSpawner : MonoBehaviour
 {
-    public string reminder = "";
+    public string reminder;
+    public string reminder1;
+    private string timePassed;
     public GameObject reminderBoard;
-    public float myTime = 10;
+    public Text timeText;
+    public int myTime = 10;
     public float current;
+    public int newTime = 30;
 
     void Start()
     {
-        //reminderBoard.SetActive(false);
-        //reminder = GetComponent<Text>().text;
-        current = myTime * 10f;
+        reminderBoard.SetActive(false);
+        current = myTime + 35;
+        reminder = "Вы находитесь за компьютером уже ";
+        reminder1 = reminder;
+        timePassed = "30";
     }
     
     void Update()
     {
         current = current - Time.deltaTime;
+        if ((current <= 35) & (current > 30))
+        {
+            reminder = reminder1 + ' ' + timePassed + " минут!";
+            timeText.text = reminder;
+            reminderBoard.SetActive(true);
+            current = 30;
+        }
+
         if (current <= 0)
         {
-            //Debug.Log("Прошло 10 секунд");
-            //reminder = reminder + " 30 минут!";
-            //GetComponent<Text>().text = reminder;
-            reminderBoard.SetActive(true);
-            current = myTime * 1000f;
+            reminderBoard.SetActive(false);
+            current = myTime + 35;
+            newTime += 30;
+            timePassed = newTime.ToString();
         }
      
     }
