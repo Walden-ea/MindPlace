1c53907f3e91d97b874fa4b04c4d7a61f66330f4
nastya nenapisannoe <nenapisannoe@yandex.ru>
2020-12-04 13:19:11 +0300

    fixed cards

--- Assets/Scripts/CreateCard.cs
+++ Assets/Scripts/CreateCard.cs
@@ -9,7 +9,8 @@ public class CreateCard : MonoBehaviour
    [SerializeField] GameObject cardPrefab;
    [SerializeField] GameObject Window;
 
-   [SerializeField]InputField field;
+   [SerializeField]InputField FirstSideInput;
+   [SerializeField]InputField SecondSideInput;
     void Start()
     {
        Window.SetActive(false);
@@ -19,34 +20,18 @@ public class CreateCard : MonoBehaviour
     {
         GameObject newCard = Instantiate(cardPrefab, new Vector3(2.0F, 0, 0), Quaternion.identity) as GameObject;
         GameObject textSideOne = GameObject.Find("FirstSideText");
-        textSideOne.GetComponent<TextMeshPro>().text = field.text;
+        textSideOne.GetComponent<TextMeshPro>().text = FirstSideInput.text;
+        GameObject textSideTwo = GameObject.Find("SecondSideText");
+        textSideTwo.GetComponent<TextMeshPro>().text = SecondSideInput.text;        
+
     }
 
-    public void FillCard(GameObject card)
-    {
-        GameObject textSideOne = GameObject.Find("FirstSideText");
-        textSideOne.GetComponent<Text>().text = field.text;
-    }
     public void Awake()
     {
         Window.SetActive(true);
     }
-    public void Show(string inputString)
-    {
 
-          //inputFieldSideOne.text = inputString;
-          //Debug.Log(inputFieldSideOne.text);
 
-    }
-    void Update()
-    {
-        
-    }
-
-    void CreateNewCard()
-    {
-        
-    }
 
 
 }
