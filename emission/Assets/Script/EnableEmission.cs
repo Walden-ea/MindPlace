11a003b0568cd05f0155e510c9830f5443ed976d
AshGir <aaaa>
2020-12-04 18:30:00 +0300

    material git add .

---
+++ emission/Assets/Script/EnableEmission.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableEmission : MonoBehaviour
+{
+
+    public bool emis;
+    private MeshRenderer ren;
+    public Material MaterialEmissionOn;
+    public Material MaterialEmissionOff;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Renderer ren = GetComponent<MeshRenderer>();
+        ren.material = MaterialEmissionOff;
+        emis = false;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Renderer ren = GetComponent<MeshRenderer>();
+        
+        if(emis)      
+            ren.material = MaterialEmissionOn;        
+        else 
+            ren.material = MaterialEmissionOff;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))    // изменить клавишу       
+            emis = true;
+        
+        if(Input.GetKeyUp(KeyCode.Space))      // изменить клавишу   
+            emis = false;
+        
+
+
+
+    }
+}
