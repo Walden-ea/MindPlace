8bb73173bb02aeae392b7bc76353819f3cef6365
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2021-04-06 21:58:58 +0300

    updated

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;// Start is called before the first frame update
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.pitch = s.pitch;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+
+        }
+    }
+
+   
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, x => x.name == name);
+        if (s!=null)
+        {
+        s.source.Play();
+        }
+        else {
+            Debug.LogWarning("Audio "+name+" not found");
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptsForBook/MoveToThePreviousPage.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToThePreviousPage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject writer1;
+    public GameObject writer2;
+    public GameObject writer3;
+    public GameObject writer4;
+    public GameObject writer5;
+    public GameObject writer6;
+    public GameObject writer7;
+    public GameObject writer8;
+    public GameObject writer9;
+    public GameObject writer10;
+
+    public void MoveBack()
+    {
+        if (writer3.activeSelf)
+        {
+            writer3.SetActive(false);
+            writer4.SetActive(false);
+            writer1.SetActive(true);
+            writer2.SetActive(true);
+        }
+        else if (writer5.activeSelf)
+        {
+            writer5.SetActive(false);
+            writer6.SetActive(false);
+            writer3.SetActive(true);
+            writer4.SetActive(true);
+        }
+        else if (writer7.activeSelf)
+        {
+            writer7.SetActive(false);
+            writer8.SetActive(false);
+            writer5.SetActive(true);
+            writer6.SetActive(true);
+        }
+        else if (writer9.activeSelf)
+        {
+            writer9.SetActive(false);
+            writer10.SetActive(false);
+            writer7.SetActive(true);
+            writer8.SetActive(true);
+        }
+    }
+}
