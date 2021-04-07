57ffe2d509006751c9c9337f343f32e0af71211b
Polina17-12 <gaidina@sfedu.ru>
2021-04-07 22:44:28 +0300

    Timers and stopwatch

---
+++ Assets/Scripts/HoverOutline.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverOutline : MonoBehaviour
+{
+
+    public GameObject Object;
+
+    private void OnMouseOver() {
+        var outline = Object.AddComponent<Outline>();
+
+    outline.OutlineMode = Outline.Mode.OutlineAll;
+    outline.OutlineColor = Color.yellow;
+    outline.OutlineWidth = 5f;
+    }
+
+
+}
---
+++ ForWatch/PomoTimer/Assets/minimenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public void SettingsPanel()
+    {
+        settingsPanel.SetActive(true);
+
+    }
+
+    public GameObject breakPanel;
+    public void BreakPanel()
+    {
+        breakPanel.SetActive(true);
+
+    }
+    public void Exit()
+    {
+        settingsPanel.SetActive(false);
+
+    }
+    public void Exit1()
+    {
+        breakPanel.SetActive(false);
+
+    }
+    public GameObject stopwatch;
+    public void StopWatch()
+    {
+        stopwatch.SetActive(true);
+
+    }
+    public void Exit2()
+    {
+        stopwatch.SetActive(false);
+
+    }
+}
