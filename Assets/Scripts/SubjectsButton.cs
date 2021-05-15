54160f09b77a5eb99bbde4243814e395b2b2d3ce
nenapisannoe <58001221+nenapisannoe@users.noreply.github.com>
2021-05-15 19:08:02 +0300

    fixed array

--- Assets/Scripts/SubjectsButton.cs
+++ Assets/Scripts/SubjectsButton.cs
@@ -6,18 +6,22 @@ public class SubjectsButton : MonoBehaviour
 {
     MeshRenderer meshRender;
     public Texture[] text = new Texture[4];
+
+    int cur = 0;
     
     void Start()
     {
         meshRender = GetComponent<MeshRenderer>();
     }
 
+
    
      public void sub()
     {
-        meshRender.material.SetTexture("_MainTex", text[0]);
+        meshRender.material.SetTexture("_MainTex", text[cur%4]);
+        cur++;
     }
-    public void sub1()
+    /*public void sub1()
     {
         meshRender.material.SetTexture("_MainTex", text[1]);
     }
@@ -28,5 +32,5 @@ public class SubjectsButton : MonoBehaviour
     public void sub3()
     {
         meshRender.material.SetTexture("_MainTex", text[3]);
-    }
+    } */
 }
