ce30c1b603717b7dd4014d542ef693dde4c76293
vadimka-8888 <73032993+vadimka-8888@users.noreply.github.com>
2021-05-16 21:17:30 +0300

    Напоминания
    Я сделал появляющиеся напоминания, но т.к. у меня что-то не так с разрешением, мне было довольно трудно всё это проконтролировать. Я немного там поперемещал юи, но старался ничего лишнего не трогать. По идее должно работать. Сейчас уведомления вылезают на 30 секунд каждые 10 секунд (чтобы во время проверки работы не ждать). Чтобы уведомления отображались каждые 30 минут нужно в переменной MyTime в скрипте и в редакторе поставить не 10, 1800. Также уведомления регулируются галочкой из настроек.

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -1,12 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
-public class Buttons : MonoBehaviour
+public class buttons : MonoBehaviour
 {
-    [SerializeField] GameObject Window;
-    public void Awake()
-    {
-        Window.SetActive(true);
+    public Text pauseText;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+     public void pause_button()
+    { 
+        if (Time.timeScale != 0)
+        { 
+            Time.timeScale = 0;
+            pauseText.text = "продолжить";
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseText.text = "пауза";
+        } 
+        
     }
 }
---
+++ Assets/Scripts/ReminderSpawner.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+//using System.Threading;
+
+public class ReminderSpawner : MonoBehaviour
+{
+    public string reminder;
+    public string reminder1;
+    private string timePassed;
+    public GameObject reminderBoard;
+    public Text timeText;
+    public int myTime = 10;
+    public float current;
+    public int newTime = 30;
+    public bool tog = true;
+
+    void Start()
+    {
+        reminderBoard.SetActive(false);
+        current = myTime + 35;
+        reminder = "Вы находитесь за компьютером уже ";
+        reminder1 = reminder;
+        timePassed = "30";
+    }
+
+    void Update()
+    {
+        current = current - Time.deltaTime;
+        if ((current <= 35) & (current > 30))
+        {
+            reminder = reminder1 + ' ' + timePassed + " минут!";
+            timeText.text = reminder;
+            if (tog)
+                reminderBoard.SetActive(true);
+            current = 30;
+        }
+
+        if (current <= 0)
+        {
+            reminderBoard.SetActive(false);
+            current = myTime + 35;
+            newTime += 30;
+            timePassed = newTime.ToString();
+        }
+
+    }
+
+    public void StopToRemind()
+    {
+        if (tog == true)
+            tog = false;
+        else tog = true;
+    }
+
+}
+
