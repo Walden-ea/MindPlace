732645ce3c11e4c6bc5c93604120ef221e2f2ef8
Elena Ignatenko <Walden-ea@yandex.ru>
2021-05-16 22:38:42 +0300

    движения камеры -- все!

--- Assets/Scripts/CameraScripts/CameraMovement.cs
+++ Assets/Scripts/CameraScripts/CameraMovement.cs
@@ -10,8 +10,13 @@ public class CameraMovement : MonoBehaviour
    static public bool moving = false;
 
    public GameObject endMarker;
+   public GameObject startMarker;
    private Vector3 _endMarker;
-   //public Camera camera;
+   Vector3 rotationDifference;
+
+    static public bool isCloseUp = false;
+    [SerializeField] GameObject exitButton;
+    
 
    float speed;
    float rotationSpeed = 1.7f;
@@ -22,40 +27,31 @@ public class CameraMovement : MonoBehaviour
         moving = true;
     }
 
+        public void slideBack()
+    {
+        if (isCloseUp)
+        {
+            moving = true;
+        }
+
+    }
+
     void Update()
     {
-        if (moving)//Плавное "скольжение" из одной точки в другую
+        if (moving&&(!isCloseUp))//Плавное "скольжение" из одной точки в другую
         {
-            Debug.Log("should move");
-            /*float rotateX = Random.Range (0, 50);
-            Debug.Log("should move");
-            //transform.position = Vector3.Lerp (transform.position, destinationPoint, smoothing * Time.deltaTime);
-            Vector3 lerp = new Vector3(20f, 180f, -1f);
-            //transform.eulerAngles = lerp;
-            transform.Rotate(lerp, 10.0f * Time.deltaTime);
-            Debug.Log("MOVE ALREADY");*/
 
             float distance = Vector3.Distance(transform.position,endMarker.transform.position);
-
             speed = distance*0.7f;
             speed = Mathf.Pow(speed,1.5f);
-    
-
             if (speed<0.7f) 
             {
                 speed = 0.7f;
             }
-
             transform.position = Vector3.MoveTowards(transform.position,
             _endMarker,
             1.7f*speed* Time.deltaTime);
-
-           // transform.rotation = Quaternion.Slerp(transform.rotation,new Quaternion(0,1,0,0), timeCount);
-            //timeCount += Time.deltaTime;
-
-            //camera.transform.rotation = new Quaternion(0,1,0,0);
-
-            Vector3 rotationDifference =new Vector3(0f,180f,0f) - transform.rotation.eulerAngles;
+            rotationDifference =new Vector3(0f,180f,0f) - transform.rotation.eulerAngles;
             if ((Mathf.Abs(rotationDifference.x-transform.position.x)>0.4f)||
             ((Mathf.Abs(rotationDifference.y-transform.position.y)>0.4f)||
             (Mathf.Abs(rotationDifference.z-transform.position.z)>0.4f)))
@@ -66,15 +62,48 @@ public class CameraMovement : MonoBehaviour
             {
                 Debug.Log("stopped moving");
                 moving = false;
+                isCloseUp = true;
+                exitButton.SetActive(true);
+                rotationDifference =startMarker.transform.rotation.eulerAngles - transform.rotation.eulerAngles;
             }
+        } else if (isCloseUp&&moving)
+        {
+
+            Debug.Log(transform.position);
+            Debug.Log(startMarker.transform.position);
+            float distance = Vector3.Distance(transform.position,startMarker.transform.position);
+            Debug.Log(distance);
+            speed = distance*0.7f;
+            speed = Mathf.Pow(speed,1.5f);
+            if (speed<0.7f) 
+            {
+                speed = 0.7f;
+            }
+            transform.position = Vector3.MoveTowards(transform.position,
+            startMarker.transform.position,
+            1.7f*speed* Time.deltaTime);
+
+            transform.rotation = startMarker.transform.rotation;
+
+            if (distance==0f)
+            {
+                Debug.Log("stopped moving");
+                moving = false;
+                isCloseUp = false;
+                exitButton.SetActive(false);
+            }
+           
         }
     }
 
     void Start()
     {
-        
+         exitButton.SetActive(false);
         _endMarker = endMarker.transform.position;
         _endMarker.x -=0f;
+        exitButton.SetActive(false);
+
+        //_startPosition.position = new Vector3(-5.76580858f,4.95239925f,5.67132282f);
     }
 
 }
--- Assets/Scripts/Sticker.cs
+++ Assets/Scripts/Sticker.cs
@@ -20,27 +20,32 @@ public class Sticker : MonoBehaviour
         var rand = Random.Range(0,17);
         //meshRender = GetComponent<MeshRenderer>();
        //meshRender.material.SetTexture("_MainTex", text[rand]);
-       transform.FindChild("paper.007").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", text[rand]);
+       transform.Find("paper.007").GetComponent<MeshRenderer>().material.SetTexture("_MainTex", text[rand]);
 
     }
 
 
     private void OnMouseDown() {
-       if(!created)
+        //Debug.Log(CameraMovement.isCloseUp);
+        if (CameraMovement.isCloseUp)
         {
-           
-            newcanv = Instantiate(inputfield, transform.position, transform.rotation);
+        if(!created)
+            {
+            
+                newcanv = Instantiate(inputfield, transform.position, transform.rotation);
 
-            newcanv.transform.SetParent(GameObject.FindGameObjectWithTag("canvas").transform, false);
-            newcanv.SetActive(true);
-            created = true;
+                newcanv.transform.SetParent(GameObject.FindGameObjectWithTag("canvas").transform, false);
+                newcanv.SetActive(true);
+                created = true;
+            }
+            else
+            {
+                newcanv.SetActive(true);
+            }
         }
-        else
-        {
-            newcanv.SetActive(true);
+        else {
+            CameraMovement.moving = true;
         }
-
-       //CameraMovement.moving = true;
         
      }
 
